52b36254551bfeb0ab3bf45bdc1e657bd73a0e4b
Seungwon <lsw010806@naver.com>
2023-10-21 06:40:30 +0900

    Login Example used if and else if, else

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('HEROPY!');
+const userID = "Admin";
+const userPW = "TheMaster";
+
+const inputID = prompt("Please Login");
+
+if (inputID == userID) {
+		const inputPW = prompt("Please Password");
+		if (inputPW == userPW) {
+				alert("Welcome!");
+		} else if (inputPW == '' | inputPW == null){
+				alert("exit...");
+		} else {
+				alert("Authentication failed...!");
+		}
+} else if (inputID == '' || inputID == null) {
+		alert("exit...");
+} else {
+		alert("Not found User...");
+}
+
